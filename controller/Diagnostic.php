04355484b82928c1d419a7986b272ef6df68d11f
antoinerobin <antoine.robin.2014@utt.fr>
2017-04-12 14:21:28 +0200

    provide a list of deliveries that can be tried and a way to do it

--- controller/Diagnostic.php
+++ controller/Diagnostic.php
@@ -20,10 +20,13 @@
 
 namespace oat\ltiClientdiag\controller;
 
+use oat\ltiClientdiag\model\ImpersonatingService;
+use oat\tao\model\SessionSubstitutionService;
 use \oat\taoClientDiagnostic\controller\Diagnostic as DiagnosticController;
 use common_session_SessionManager as SessionManager;
 use oat\oatbox\service\ServiceNotFoundException;
 use oat\taoClientDiagnostic\model\storage\Storage;
+use oat\taoTestTaker\models\TestTakerService;
 use \taoLti_models_classes_LtiLaunchData as LtiLaunchData;
 
 /**
@@ -40,13 +43,16 @@ class Diagnostic extends DiagnosticController
     {
         $diagnostics = $this->getDiagnosticDataTable()->getDiagnostics($this->getRequestOptions());
 
+        $deliveryUrl = $this->getServiceManager()->has(ImpersonatingService::SERVICE_ID)?_url('deliveries'):'';
+
         $data = array(
             'title'  => __('Readiness diagnostics'),
             'set'    => json_encode($diagnostics),
             'config' => json_encode($this->loadConfig()),
             'action' => 'index',
             'controller' => 'Diagnostic',
-            'extension' => 'ltiClientdiag'
+            'extension' => 'ltiClientdiag',
+            'deliveryurl' => $deliveryUrl
         );
 
         $userLabel = SessionManager::getSession()->getUserLabel();
@@ -106,6 +112,100 @@ class Diagnostic extends DiagnosticController
         $contextId = $session->getLaunchData()->getVariable(LtiLaunchData::CONTEXT_ID);
         $options['filter'] = [Storage::DIAGNOSTIC_CONTEXT_ID => $contextId];
         return $options;
+    }
+
+    public function deliveries()
+    {
+
+        $data = array(
+            'action' => 'deliveries',
+            'controller' => 'Diagnostic',
+            'extension' => 'ltiClientdiag',
+            'deliveries' => json_encode($this->getDeliveryData())
+        );
+
+        $this->defaultData();
+        $this->setData('userLabel', SessionManager::getSession()->getUserLabel());
+        $this->setData('cls', 'delivery-list');
+        $this->setData('data', $data);
+        $this->setData('content-template', 'pages/index.tpl');
+        $this->setData('content-template-ext', 'taoClientDiagnostic');
+        $this->setView('layout.tpl');
+    }
+
+    private function getDeliveryData()
+    {
+        $deliveryData = [];
+
+        if($this->getServiceManager()->has(ImpersonatingService::SERVICE_ID)){
+            /** @var ImpersonatingService $service */
+            $service = $this->getServiceManager()->get(ImpersonatingService::SERVICE_ID);
+            $deliveries = $service->getDeliveries();
+            foreach ($deliveries as $delivery){
+                $deliveryData[] = array(
+                    'text' => __('Try this delivery'),
+                    'label' => $delivery->getLabel(),
+                    'url' => _url('passTest', null,null, array('uri' => $delivery->getUri()))
+                );
+            }
+
+        }
+
+        return $deliveryData;
+
+    }
+
+    /**
+     * Allow a lti user to pass a test as a test taker
+     */
+    public function passTest()
+    {
+
+        if(!$this->hasRequestParameter('uri')){
+            throw new \common_exception_MissingParameter('uri');
+        }
+
+        try{
+            /** @var ImpersonatingService $service */
+            $service = $this->getServiceManager()->get(ImpersonatingService::SERVICE_ID);
+            $redirectUrl = $service->getLaunchUrl($this->getRequestParameter('uri'));
+
+            /** @var SessionSubstitutionService $sessionSubstitutionService */
+            $sessionSubstitutionService = $this->getServiceManager()->get(SessionSubstitutionService::SERVICE_ID);
+
+            $testTakerService = TestTakerService::singleton();
+            $student = TestTakerService::singleton()->createInstance($testTakerService->getRootClass(), SessionManager::getSession()->getUserLabel());
+            $testTakerService->setTestTakerRole($student);
+            $user = new \core_kernel_users_GenerisUser($student);
+            $sessionSubstitutionService->substituteSession($user);
+        } catch(ServiceNotFoundException $e){
+            $redirectUrl = $this->redirect(_url('deliveries'));
+        }
+
+
+        $this->redirect($redirectUrl);
+
+    }
+
+
+    /**
+     * Return to the lti view of the deliveries
+     * @throws \tao_models_classes_AccessDeniedException
+     */
+    public function returnToLti()
+    {
+        /** @var SessionSubstitutionService $sessionSubstitutionService */
+        $sessionSubstitutionService = $this->getServiceManager()->get(SessionSubstitutionService::SERVICE_ID);
+        if ($sessionSubstitutionService->isSubstituted()) {
+            $user = SessionManager::getSession()->getUser();
+            $sessionSubstitutionService->revert();
+            $testTakerService = TestTakerService::singleton();
+            $testTakerService->deleteResource(new \core_kernel_classes_Resource($user->getIdentifier()));
+        } else {
+            throw new \tao_models_classes_AccessDeniedException(SessionManager::getSession()->getUserUri(), __METHOD__, __CLASS__, 'taoClientDiagnostic');
+        }
+
+        $this->redirect(_url('deliveries'));
     }
 
 }
