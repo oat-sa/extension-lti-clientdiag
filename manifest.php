742599a6078cad1c89eed3e84f4da413bbcb2848
Sergii <sergii@taotesting.com>
2018-02-19 10:38:00 +0100

    Bumped version

--- manifest.php
+++ manifest.php
@@ -28,10 +28,10 @@ return [
     'label' => 'LTI Client Diagnostic',
     'description' => 'Grants access to the client diagnostic functionality using LTI',
     'license' => 'GPL-2.0',
-    'version' => '1.3.3',
+    'version' => '1.4.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
-        'taoLti' => '>=4.2.0',
+        'taoLti' => '>=5.0.0',
         'taoClientDiagnostic' => '>=2.5.0',
     ],
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#ltiClientdiagManager',
